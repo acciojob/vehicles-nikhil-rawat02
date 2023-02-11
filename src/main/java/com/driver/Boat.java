167778236b16e803b8c4150323ecf4bc15a221e1
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-02-11 11:58:44 +0530

    updated boat class and add getter and setter in car class

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,24 +1,14 @@
 package com.driver;
 
 public class Boat implements WaterVehicle{
-    private String name;
-    private int capacity;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
 
     @Override
     public String getVehicleName() {
-        return this.name;
+        return name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return this.capacity;
+        return capacity;
     }
 }
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -1,6 +1,9 @@
 package com.driver;
 
 public interface WaterVehicle {
+    public static final String name = "name";
+    public static final int capacity = 0;
+
     public String getVehicleName();
     public int getVehicleCapacity();
 }
